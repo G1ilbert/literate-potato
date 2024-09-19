2d274fd52c9eccd4e0b5953d18153cf94ff8eb1b
G1ilbert <163719273+G1ilbert@users.noreply.github.com>
2024-09-20 05:03:09 +0700

    Update Main.java

--- Lab8_2/src/Packgage/Main.java
+++ Lab8_2/src/Packgage/Main.java
@@ -1,33 +1,14 @@
 package Packgage;
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.util.Iterator;
 
-public class Main implements ActionListener{
-	private int clickCount = 0;
-
-	public static void main(String[] args) { 
-       	JFrame frame = new JFrame("Test"); 
-        frame.setSize(250, 300); 
-        frame.setVisible(true); 
-        frame.setEnabled(true);
-        
-        JButton jb = new JButton("click");
-        JPanel center = new JPanel();
-        center.add(jb);
-        frame.getContentPane().add(center, BorderLayout.CENTER); 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        Main click = new Main();
-        jb.addActionListener(click);
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		clickCount++;
-		
-		JButton cc = (JButton) e.getSource();
-		cc.setText(clickCount+"");
-	}
+public class Main {
+	
+    public static void Main(String[] args) {
+        NameRepository namesRepository = new NameRepository();
+        for (Iterator iter = namesRepository.getIterator(); iter.hasNext();) {
+            String name = (String)iter.next();
+            System.out.println("Name : " + name);
+        }
+    }
 }
